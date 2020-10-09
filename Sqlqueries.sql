d31a7d9fd8a5f87518133713da6cb624222b35a1
Isha5 <45407826+Isha5@users.noreply.github.com>
2020-10-09 14:10:18 +0200

    Update Sqlqueries.sql

--- Sqlqueries.sql
+++ Sqlqueries.sql
@@ -16,11 +16,16 @@ select count(*) from population_years where country like '%Islands%';
 select population from population_years where country = 'Indonesia' and year = '2000';
 select population from population_years where country = 'Indonesia' and year = '2010';
 
+SELECT population, year FROM population_years
+ WHERE country = 'Indonesia' 
+ AND year BETWEEN 2000 AND 2011
+ ORDER BY year asc;
+
 
 SELECT country,
  CASE
   WHEN population > 100 THEN 'Highly_populous'
-  WHEN imdb_rating < 10 THEN 'Less_populous'
+  WHEN population < 10 THEN 'Less_populous'
   ELSE 'Moderate Growth'
- END AS Population_Growth_Slope
+ END AS 'Population_Growth_Slope'
 FROM population_years;
